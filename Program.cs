d555cfa7af1850376e0cf47c34259c677ab1a541
rifqigentabuana <rifqigentabuana@students.amikom.ac.id>
2021-03-18 02:26:05 +0700

    commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari class Taxi
+            Taxi Driver1 = new Taxi();
+            Taxi Driver2 = new Taxi();
+
+            // setting nilai properties objek aing dan koe
+            Driver1.Nama = "SUPRIADI";
+            Driver1.Status = "AVAILABLE";
+            Driver1.Jml_Penumpang = 42;
+            Driver1.Jml_Penumpang1 = 0;
+
+            Driver2.Nama = "BAMBANG";
+            Driver2.Status2 = "NOT AVAILABLE";
+            Driver2.Jml_Penumpang = 54;
+            Driver2.Jml_Penumpang1 = 0;
+
+            // memanggil method registrasi dan isi krs
+            Driver1.Info_Driver();
+            Driver1.Status_Driver();
+
+            Driver2.Info_Driver2();
+            Driver2.Status_Driver2();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TugasClass
+
+{
+	public class Taxi
+	{
+		// properties
+		public string Status { get; set; }
+		public string Status2 { get; set; }
+		public string Nama { get; set; }
+		public string Nama2 { get; set; }
+		public int Jml_Penumpang { get; set; }
+		public int Jml_Penumpang1 { get; set; }
+
+		// method
+		// note. disini saya asumsi-kan "penurunan penumpang" = tendang keluar dari taxi karena hal-hal tertentu
+		public void Info_Driver()
+		{
+			Console.WriteLine("\nTAXI ONLINE DABEST");
+			Console.WriteLine("Nama: {0}", Nama);
+			Console.WriteLine("Status: {0}", Status);
+			Console.WriteLine("Total Penumpang Hari Ini: {0}", Jml_Penumpang);
+
+		}
+		public void Status_Driver()
+		{ 
+			Console.WriteLine("{0}\n", Status);
+			Console.WriteLine("Total Jumlah Pengambilan Penumpang :{0}\n", Jml_Penumpang);
+			Console.WriteLine("Total Jumlah Penurunan Penumpang :{0}\n", Jml_Penumpang1);
+		}
+		public void Info_Driver2()
+		{
+			Console.WriteLine("\nTAXI ONLINE DABEST");
+			Console.WriteLine("Nama: {0}", Nama);
+			Console.WriteLine("Status: {0}", Status2);
+			Console.WriteLine("Total Penumpang Hari Ini: {0}", Jml_Penumpang);
+
+		}
+		public void Status_Driver2()
+		{
+			Console.WriteLine("{0}\n", Status2);
+			Console.WriteLine("Total Jumlah Pengambilan Penumpang :{0}\n", Jml_Penumpang);
+			Console.WriteLine("Total Jumlah Penurunan Penumpang :{0}\n", Jml_Penumpang1);
+		}
+	}
+}
